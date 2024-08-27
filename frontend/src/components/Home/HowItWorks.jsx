23700a11170b6b5a0f6eac8e188f5b87edc84aaf
Saurabh25032002 <sarahpanwar992@gmail.com>
2024-08-27 12:53:37 +0530

    first commit

---
+++ frontend/src/components/Home/HowItWorks.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+import { MdFindInPage } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
+
+const HowItWorks = () => {
+  return (
+    <>
+      <div className="howitworks">
+        <div className="container">
+          <h3>How JobZee Works</h3>
+          <div className="banner">
+            <div className="card">
+              <FaUserPlus />
+              <p>Create Account</p>
+              <p>
+              Create an account to explore job opportunities or post openings. Start your journey with us today!
+              </p>
+            </div>
+            <div className="card">
+              <MdFindInPage />
+              <p>Find a Job/Post a Job</p>
+              <p>
+              Find your ideal job or post openings to attract top talent. Simplify your search or hiring process now!
+              </p>
+            </div>
+            <div className="card">
+              <IoMdSend />
+              <p>Apply For Job/Recruit Suitable Candidates</p>
+              <p>
+              Apply for your dream job or recruit suitable candidates quickly and efficiently. Take the next step today!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HowItWorks;
